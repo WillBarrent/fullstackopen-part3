ee9d251e4ca1aeb25658bd722992735884440861
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2026-04-24 23:36:25 +0500

    Add env for PORT

--- phonebook/backend/index.js
+++ phonebook/backend/index.js
@@ -96,7 +96,7 @@ app.get("/info", (req, res) => {
     <p>${date}</p>`);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
 });
