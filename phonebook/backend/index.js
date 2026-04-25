438c11fa25d716771d613e06dd66d63cdec71052
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2026-04-25 21:19:40 +0500

    Exercises 3.13-3.18: Forgot to add errorHandler to use

--- phonebook/backend/index.js
+++ phonebook/backend/index.js
@@ -128,6 +128,8 @@ const errorHandler = (error, req, res, next) => {
   next(error);
 };
 
+app.use(errorHandler);
+
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
